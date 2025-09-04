978da08786149126e0e42967fde9fd00ad3d2823
danutavram <avramnicolaedan@gmail.com>
2025-09-04 17:01:34 +0300

    Teams

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -13,7 +13,7 @@ const ServiceCard = ({service, index}) => {
     }
 
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10' 
+    <div className='relative overflow-hidden max-w-lg h-min m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10' 
     onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
         <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] 
         absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`} style={{top: position.y - 90, left: position.x - 90}}/>

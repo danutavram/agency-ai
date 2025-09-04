f618bb6677f681419b79321732e2c89b68fcc81a
danutavram <avramnicolaedan@gmail.com>
2025-09-04 16:34:32 +0300

    Services&ServiceCard

---
+++ src/components/OurWork.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurWork = () => {
+  return (
+    <div id='our-work' className='flex'>
+
+    </div>
+  )
+}
+
+export default OurWork
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
 
@@ -16,6 +17,11 @@ const Services = () => {
             icon: assets.marketing_icon,
         },
         {
+            title: 'Content writing',
+            description: 'We turn bold ideas into powerful digital solutions that connect...',
+            icon: assets.content_icon,
+        },
+        {
             title: 'Social media',
             description: 'We help you build a strong social media presence and engage with your audience.',
             icon: assets.social_icon,
@@ -26,6 +32,12 @@ const Services = () => {
     <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
         <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward' />
+
+        <div className='flex flex-col md:grid grid-cols-2 gap-6 md:gap-8'>
+            {servicesData.map((service, index)=>(
+                <ServiceCard key={index} service={service} index={index}/>
+            ))}
+        </div>
 
     </div>
   )     

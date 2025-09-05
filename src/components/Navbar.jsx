ba143e0e3d756c2230ade6582299be16738fb316
danutavram <avramnicolaedan@gmail.com>
2025-09-05 11:01:24 +0300

    motion framer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import assets from '../assets/assets'
 import ThemeToggleBtn from './ThemeToggleBtn'
+import {motion} from 'motion/react'
 
 const Navbar = ({theme, setTheme}) => {
 
     const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
-    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky 
+    <motion.div
+    initial={{opacity: 0, y: -50}}
+    animate={{opacity: 1, y: 0}}
+    transition={{duration: 0.6, ease: 'easeOut'}}
+    className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky 
     top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
         <img onClick={()=> window.scrollTo({top:0, behavior:'smooth'})} src={theme === 'dark' ? assets.logo_dark : assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' />
         <div className={`text-gray-700 dark:text-white sm:text-sm ${!sidebarOpen ? 'max-sm:w-0 overflow-hidden' : 'max-sm:w-60 max-sm:pl-10'}
@@ -27,7 +32,7 @@ const Navbar = ({theme, setTheme}) => {
                 Connect <img src={assets.arrow_icon} width={14} alt="" />
             </a>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
